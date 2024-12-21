0e40c1941d5bb72103cfb42f283d114c96074beb
pantokrator4 <mralexfour@gmail.com>
2024-12-21 20:38:49 +0400

    0.4.4 ProgressLines refactor

--- src/components/ProgressLines.tsx
+++ src/components/ProgressLines.tsx
@@ -15,18 +15,40 @@ export const ProgressLine = (props: lineNamePropsType) => {
     return (
         <StyledProgressLineDiv >
         <h3>{props.lineName}</h3>
-        <Icon iconId = {props.iconId} viewBox = {props.viewBox}  width= {props.width} height={props.height}/>
+        <BackgroundLine>
+            <InsideLine/>
+        </BackgroundLine>
+        {/* <Icon iconId = {props.iconId} viewBox = {props.viewBox}  width= {props.width} height={props.height}/> */}
         </StyledProgressLineDiv>
     )
 }
 
 const StyledProgressLineDiv = styled.div`
-    
+    display: flex;
+    flex-direction:column;
+    max-width: 900px;
+    width: 100%;
     h3 {
         margin-bottom: 5px;
         display: flex;
         ${font({Fmax: 24, Fmin: 16, weight: 600})}
     }
+`
+
+const BackgroundLine = styled.div`
+    display: flex;
+    height: 18px;
+    background-color: #162950;
+    border-radius: 10px;
+`
+
+const InsideLine = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    background: linear-gradient(270deg, #13adc7 0%, #6978d1 66.67%, #945dd6 100%);
+    width: 50%;
+    height: 18px;
+    border-radius: 10px;
 `
 
 
