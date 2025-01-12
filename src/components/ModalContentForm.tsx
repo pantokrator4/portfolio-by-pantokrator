a6aa6bae5cbaf2b2275e8cfa120d8990971286d0
pantokrator4 <mralexfour@gmail.com>
2025-01-12 17:21:34 +0400

    0.5.72 modal window fixes

--- src/components/ModalContentForm.tsx
+++ src/components/ModalContentForm.tsx
@@ -2,12 +2,14 @@ import styled from "styled-components";
 import { Button } from "./Button";
 import { theme } from "../styles/Theme";
 import emailjs from '@emailjs/browser';
-import { ElementRef, useRef } from "react";
+import { ChangeEvent, ElementRef, useRef, useState } from "react";
 import { toast } from 'react-toastify';
 
 type ModalContentFormProps = {
   onClose: () => void;
 };
+
+
 
 export const ModalContentForm = ({onClose}: ModalContentFormProps) => {
   const form = useRef<ElementRef<"form">>(null);
@@ -20,13 +22,14 @@ export const ModalContentForm = ({onClose}: ModalContentFormProps) => {
 
     emailjs
       .sendForm('service_yxomwra', 'template_bp0xuhf', form.current, {
-        publicKey: 'YzKVRb6YA6hli78c2',
+        publicKey: 'YzKVRb6YA6hli78c2'
       })
       .then(
         () => {
           console.log('SUCCESS!');
           notify();
           onClose();
+          onClickButtonHandler();
         },
         (error) => {
           console.log('FAILED...', error.text);
@@ -34,15 +37,50 @@ export const ModalContentForm = ({onClose}: ModalContentFormProps) => {
       );
   };
 
+  const [inputFieldState1, setInputFieldState1] = useState("")
+  const [inputFieldState2, setInputFieldState2] = useState("")
+  const [inputFieldState3, setInputFieldState3] = useState("")
+  const [inputFieldState4, setInputFieldState4] = useState("")
+
+  const onChangeHandler1 = (event: ChangeEvent<HTMLInputElement>) => {
+    const updatedInput = event.currentTarget.value
+    setInputFieldState1(updatedInput)
+  }
+
+  const onChangeHandler2 = (event: ChangeEvent<HTMLInputElement>) => {
+    const updatedInput = event.currentTarget.value
+    setInputFieldState2(updatedInput)
+    
+  }
+
+  const onChangeHandler3 = (event: ChangeEvent<HTMLInputElement>) => {
+    const updatedInput = event.currentTarget.value
+    setInputFieldState3(updatedInput)
+  }
+
+  const onChangeTextAreaHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const updatedTextAreaInput = event.currentTarget.value
+    setInputFieldState4(updatedTextAreaInput)
+  }
+
+  const onClickButtonHandler = () => {
+    setInputFieldState1("")
+    setInputFieldState2("")
+    setInputFieldState3("")
+    setInputFieldState4("")
+  }
+
+  
+
   return (
     <StyledModalContentForm>
       <span>Fill the form</span>
       
       <StyledForm ref={form} onSubmit={sendEmail}>
-        <Field placeholder={"Name"} type="text" required name={"user_name"}/>
-        <Field placeholder={"Subject"} type="text" required name={"subject"}/>
-        <Field placeholder={"Email"} type="email" required name={"email"}/>
-        <Field placeholder={"Message"} as={"textarea"} name={"message"}/>
+        <Field value={inputFieldState1} onChange={onChangeHandler1} placeholder={"Name"} type="text" required name={"user_name"}/>
+        <Field value={inputFieldState2} onChange={onChangeHandler2} placeholder={"Subject"} type="text" required name={"subject"}/>
+        <Field value={inputFieldState3} onChange={onChangeHandler3} placeholder={"Email"} type="email" required name={"email"}/>
+        <Field value={inputFieldState4} onChange={onChangeTextAreaHandler} placeholder={"Message"} as={"textarea"} name={"message"}/>
         <Button type={"submit"}>submit</Button>
       </StyledForm>
     </StyledModalContentForm>
